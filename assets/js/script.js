56de7c4ba302264ec2902d48f75b663fda6caf29
Yousouf Mustafa Muhammed <yousoufmostafamuslim@gmail.com>
2020-12-31 01:50:35 +0200

    navbar changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var navbar__links = document.querySelector(".navbar__menu ")
+var navbar__animation = document.querySelector("#mobile__menu ")
 
 document.querySelector('.navbar__toggle').addEventListener('click', function () {
     navbar__links.classList.toggle('active');
+    navbar__animation.classList.toggle('is-active');
 } );
