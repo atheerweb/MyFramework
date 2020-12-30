8f73aa689fa39237bea33d58bdbf091a4bdbca57
Yousouf Mustafa Muhammed <yousoufmostafamuslim@gmail.com>
2020-12-31 01:18:46 +0200

    Navbar JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,5 @@
+var navbar__links = document.querySelector(".navbar__menu ")
 
+document.querySelector('.navbar__toggle').addEventListener('click', function () {
+    navbar__links.classList.toggle('active');
+} );
