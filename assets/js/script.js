2a0817669df7afd38e17f14053d59fa2bf1e5d3d
Yousouf Mustafa Muhammed <yousoufmostafamuslim@gmail.com>
2021-01-04 16:45:14 +0200

    some fixes to the about me section

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,4 @@ var navbar__animation = document.querySelector("#mobile__menu ")
 document.querySelector('.navbar__toggle').addEventListener('click', function () {
     navbar__links.classList.toggle('active');
     navbar__animation.classList.toggle('is-active');
-} );
-
-
-
-
+});
